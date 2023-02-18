b0da5c738500d95bd97e316c3dec8b122675978c
Ruslan Musaev <ruslan.msv@gmail.com>
2023-02-18 19:27:56 +0100

    Adding design system to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      lg: "1440px",
+    },
+    colors: {
+      red: "hsl(0, 100%, 67%)",
+      yellow: "hsl(39, 100%, 56%)",
+      green: "hsl(166, 100%, 37%)",
+      "cobalt-blue": "hsl(234, 85%, 45%)",
+      white: "hsl(0, 0%, 100%)",
+      "pale-blue": "hsl(221, 100%, 96%)",
+      lavender: "hsl(241, 100%, 89%)",
+      "dg-blue": "hsl(224, 30%, 27%)",
+      "ls-blue": "hsl(252, 100%, 67%)",
+      "lr-blue": "hsl(241, 81%, 54%)",
+      "violet-blue": "hsla(256, 72%, 46%, 1)",
+      "persian-blue": "hsla(241, 72%, 46%, 0)",
+    },
+    fontFamily: {
+      hanken: ["Hanken Grotesk", "sans-serif"],
+    },
+    fontSize: {
+      sm: ["16px", { lineHeight: "21px" }],
+      md: ["18px", { lineHeight: "23px" }],
+      lg: ["24px", {lineHeight: "31px"}],
+      xl: ["32px", {lineHeight: "42px"}],
+      xxl: ["56px", {lineHeight: "72px"}],
+      '7xl': ["72px", {lineHeight: "72px"}],
+    },
+    boxShadow: {
+      'box=shadow': '0px 30px 60px rgba(61, 108, 236, 0.15)',
+    },
     extend: {},
   },
   plugins: [],
-}
+};
